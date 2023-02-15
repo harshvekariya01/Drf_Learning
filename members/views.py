c5b45c5e45582d180a521c6c686e40b7bc85061f
harsh vekariya <vekariyaharsh01@gmail.com>
2023-02-15 16:43:35 +0530

    space remove

--- members/views.py
+++ members/views.py
@@ -1,10 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import  studentSerializer
 from .models import student
-
-
-
-
 class studentviewset(ModelViewSet):
     serializer_class = studentSerializer
     queryset = student.objects.all()
